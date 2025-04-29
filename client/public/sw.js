3c9281008c6a16d7eef6e9bb09ef11e879702d70
Jonathon <jonp4208@gmail.com>
2025-04-29 06:43:03 -0400

    Increment CACHE_VERSION for deployment

--- client/public/sw.js
+++ client/public/sw.js
@@ -1,7 +1,7 @@
 // Service Worker for LD Growth PWA
 
 // Change this version number whenever you want to force an update
-const CACHE_VERSION = '1.0.76';
+const CACHE_VERSION = '1.0.77';
 const CACHE_NAME = `ld-growth-cache-${CACHE_VERSION}`;
 const urlsToCache = [
   '/',
