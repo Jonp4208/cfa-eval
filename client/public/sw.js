84984fc0525ddaacfa3cea55a9350c167a8ba1da
Your Name <your-email@example.com>
2025-06-02 14:42:56 -0400

    Increment cache version for deployment

--- client/public/sw.js
+++ client/public/sw.js
@@ -1,7 +1,7 @@
 // Service Worker for LD Growth PWA
 
 // Change this version number whenever you want to force an update
-const CACHE_VERSION = '1.0.161';
+const CACHE_VERSION = '1.0.162';
 const CACHE_NAME = `ld-growth-cache-${CACHE_VERSION}`;
 const urlsToCache = [
   '/',
